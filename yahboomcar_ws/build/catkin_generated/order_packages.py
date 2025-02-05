493623fa8531b17efdae78ac00f032bd32d1c2c6
Abraham Garcia <139540792+SON-Abe@users.noreply.github.com>
2025-02-04 19:42:37 -0800

    Add files via upload

---
+++ yahboomcar_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jetson/yahboomcar_ws/src'
+whitelisted_packages = 'yahboomcar_msgs'.split(';') if 'yahboomcar_msgs' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jetson/software/world_canvas/devel;/home/jetson/yahboomcar_ws/devel;/home/jetson/software/library_ws/devel;/home/jetson/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jetson/software/world_canvas/devel;/home/jetson/yahboomcar_ws/devel;/home/jetson/software/library_ws/devel;/home/jetson/catkin_ws/devel;/opt/ros/melodic' != '' else []
